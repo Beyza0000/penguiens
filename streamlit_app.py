b9967d6b62ee952741f917f67bddbce7e8b76732
Beyza0000 <beyzahacat@outlook.com>
2024-12-04 23:03:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import streamlit_pandas as sp
 
+@st.cache(allow_output_mutation=True)
+
+def load_data():
+    df = pd.read_csv("data/penguins_examples.csv")
+    return df
+df= load_data()
+
+
+st.write(df)
